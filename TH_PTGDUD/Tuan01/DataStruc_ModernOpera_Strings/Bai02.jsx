7310e9fcc31c7bdfbcbb535f8998da20a0b31376
KhanhNg206 <23634741.khanh@student.iuh.edu.vn>
2026-01-19 21:01:52 +0700

    bài tập 19/1/26

--- TH_PTGDUD/Tuan01/DataStruc_ModernOpera_Strings/Bai02.jsx
+++ TH_PTGDUD/Tuan01/DataStruc_ModernOpera_Strings/Bai02.jsx
@@ -1,64 +1,36 @@
-'use strict';
-
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnabry',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnabry', 'Lewandowski', 'Hummels'],
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
   odds: {
     team1: 1.33,
-    draw: 3.25,
+    x: 3.25,
     team2: 6.5,
   },
 };
 
 for (const [i, player] of game.scored.entries()) {
-  console.log(`Goal ${i + 1}: ${player}`);
+  console.log(`${player} : ${i + 1} `);
 }
 
-let averageOdd = 0;
-const oddsValues = Object.values(game.odds);
-
-for (const odd of oddsValues) {
-  averageOdd += odd;
+let sum = 0;
+for (const odd of Object.values(game.odds)) {
+  sum += odd;
 }
-averageOdd /= oddsValues.length;
-
-console.log('Average odd:', averageOdd);
+console.log(sum / Object.values(game.odds).length);
 
 for (const [key, odd] of Object.entries(game.odds)) {
-  const teamStr =
-    key === 'draw'
-      ? 'draw'
-      : `victory ${game[key]}`;
-
-  console.log(`Odd of ${teamStr}: ${odd}`);
+  const team =
+    key === 'x' ? 'draw' : `victory ${game[key]}`;
+  console.log(`Odd of ${team}: ${odd}`);
 }
+
+const scorers = {};
+
+for (const player of game.scored) {
+  scorers[player] = scorers[player]
+    ? scorers[player] + 1
+    : 1;
+}
+
+console.log(scorers);
