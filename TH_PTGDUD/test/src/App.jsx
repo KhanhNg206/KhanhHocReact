ec18c3f24df65eb38746093526f3cf4fa30b5d4f
KhanhNg206 <23634741.khanh@student.iuh.edu.vn>
2026-03-19 01:52:24 +0700

    Lý thuyết 18/3

--- TH_PTGDUD/test/src/App.jsx
+++ TH_PTGDUD/test/src/App.jsx
@@ -1,34 +1,9 @@
-import CounterReducer from "./store/CounterReducer";
-import { useSelector,useDispatch } from "react-redux";
-import useLocalStorage from "./store/useLocalStorage";
-import Users from "./store/Users";
+import QLsvComponent from "./components/QLsvComponent";
 
 function App() {
-  // const count = useSelector((state) => state.count);
-  // const dispatch = useDispatch();
-   const [name,setName] = useLocalStorage("username","");
   return (
      <>
-      {/* <h2>Count: {count}</h2>
-
-      <button onClick={() => dispatch({ type: "INCREMENT" })}>
-        Tăng
-      </button>
-
-      <button onClick={() => dispatch({ type: "DECREMENT" })}>
-        Giảm
-      </button>
-
-      <button onClick={() => dispatch({ type: "RESET" })}>
-        Reset
-      </button> */}
-
-     {/* <h2>demo</h2>
-     <input type="text" value={name}
-     onChange={(e) => setName(e.target.value)}/>
-     <p>name : {name}</p> */}
-
-     <Users/>
+    <QLsvComponent />
   </>
   );
 }
